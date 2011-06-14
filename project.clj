a1a38847e6919ac4e12ef3a55f1b64978e20d6fd
lance bradley <l@ncebradley.org>
2011-06-14 12:27:02 -0700

    0.1.5
    - use lancepantz's clj-json fork for generating sets

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject wakeful "0.1.4"
+(defproject wakeful "0.1.5"
   :description "restful routing alternative"
   :dependencies [[clojure "1.2.0"]
                  [clojure-useful "0.3.8"]
-                 [clj-json "0.3.1"]
+                 [org.clojars.lancepantz/clj-json "0.4.0"]
                  [compojure "0.6.3"]])
