cd8acd65e8ad95287546f5013fe9bdc35d286299
Alan Malloy <alan@malloys.org>
2011-06-28 12:52:21 -0700

    Depend on ego snapshot

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject wakeful "0.1.6"
+(defproject wakeful "0.1.7-SNAPSHOT"
   :description "restful routing alternative"
   :dependencies [[clojure "1.2.0"]
                  [useful "0.4.0"]
                  [org.clojars.lancepantz/clj-json "0.4.0"]
                  [compojure "0.6.3"]
-                 [ego "0.1.0"]])
+                 [ego "0.1.1-SNAPSHOT"]])
