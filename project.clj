5af559b9ecd0b23d2ccbeecd70ae0e1a872d666b
Justin Balthrop <git@justinbalthrop.com>
2011-07-13 19:40:14 -0700

    yuck!!

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject wakeful "0.2.1"
+(defproject wakeful "0.2.2"
   :description "restful routing alternative"
   :dependencies [[clojure "1.2.0"]
                  [useful "0.5.0"]
@@ -6,5 +6,6 @@
                  [compojure "0.6.3"]
                  [ego "0.1.1"]
                  [hiccup "0.3.5"]
-                 [org.clojure/tools.namespace "0.1.1"]]
+                 [org.clojure/tools.namespace "0.1.1" :exclusions [org.clojure/java.classpath]]
+                 [org.clojars.ninjudd/java.classpath "0.1.2-SNAPSHOT"]]
   :dev-dependencies [[ring "0.3.8"]]) ; for some testing
