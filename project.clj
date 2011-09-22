b55bccdca9419f57b172e8ef4ceb72785affe51d
Justin Balthrop <git@justinbalthrop.com>
2011-09-21 22:38:33 -0700

    resource-stream moved to classlojure

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject wakeful "0.2.5"
+(defproject wakeful "0.2.6"
   :description "restful routing alternative"
   :dependencies [[clojure "1.2.0"]
                  [useful "0.7.0-alpha3"]
@@ -6,6 +6,7 @@
                  [compojure "0.6.3"]
                  [ego "0.1.1"]
                  [hiccup "0.3.5"]
+                 [classlojure "0.6.1"]
                  [org.clojure/tools.namespace "0.1.1" :exclusions [org.clojure/java.classpath]]
                  [org.clojars.ninjudd/java.classpath "0.1.2-SNAPSHOT"]]
   :dev-dependencies [[ring "0.3.8"] ; for some testing
