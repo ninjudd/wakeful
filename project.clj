db473e2299ea88166790de91a1e790c5cc37f727
lance bradley <l@ncebradley.org>
2011-09-26 16:08:10 -0700

    use clj-json 0.4.1

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "restful routing alternative"
   :dependencies [[clojure "1.2.0"]
                  [useful "0.7.0-alpha3"]
-                 [clj-json "0.4.0"]
+                 [clj-json "0.4.1"]
                  [compojure "0.6.3"]
                  [ego "0.1.1"]
                  [hiccup "0.3.5"]
