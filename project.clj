65047ae627421bb29ea0021638af411e6520d360
Justin Balthrop <git@justinbalthrop.com>
2011-10-19 16:26:28 -0700

    update deps

--- project.clj
+++ project.clj
@@ -1,14 +1,12 @@
-(defproject wakeful "0.2.7"
+(defproject wakeful "0.2.8"
   :description "restful routing alternative"
   :dependencies [[clojure "1.2.0"]
-                 [useful "0.7.0-beta4"]
-                 [clj-json "0.4.1"]
+                 [useful "0.7.0"]
+                 [clj-json "0.4.2"]
                  [compojure "0.6.3"]
-                 [ego "0.1.1"]
+                 [ego "0.1.7"]
                  [hiccup "0.3.5"]
                  [classlojure "0.6.1"]
                  [org.clojure/tools.namespace "0.1.1" :exclusions [org.clojure/java.classpath]]
                  [org.clojars.ninjudd/java.classpath "0.1.2-SNAPSHOT"]]
-  :dev-dependencies [[ring "0.3.8"] ; for some testing
-                     [org.clojars.flatland/cake-marginalia "0.6.3"]]
-  :tasks [cake-marginalia.tasks])
+  :test-dependencies [[ring "0.3.8"]])
