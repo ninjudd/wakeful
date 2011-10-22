ee2d6538e326efd19524965ef219e99ee4087455
lance bradley <l@ncebradley.org>
2011-10-21 17:15:56 -0700

    remove run.clj

--- src/run.clj
+++
@@ -1,7 +0,0 @@
-(use 'ring.adapter.jetty 'wakeful.core)
-
-(def handler (wakeful "sample" :content-type "text/html"))
-
-(defn run [] (run-jetty (var handler) {:port 8080 :join? false}))
-
-(defn do-run [] (run))
