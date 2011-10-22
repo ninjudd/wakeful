737b7f9686aea520ce0bfcfc757b8bfae49278be
lance bradley <l@ncebradley.org>
2011-10-21 19:15:47 -0700

    update useful, classlojure, and compojure

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject wakeful "0.2.9"
   :description "restful routing alternative"
   :dependencies [[clojure "1.2.0"]
-                 [useful "0.7.0"]
+                 [useful "0.7.3"]
                  [clj-json "0.4.2"]
-                 [compojure "0.6.3"]
+                 [compojure "0.6.5"]
                  [ego "0.1.7"]
                  [hiccup "0.3.5"]
-                 [classlojure "0.6.1"]
+                 [classlojure "0.6.2"]
                  [org.clojure/tools.namespace "0.1.1" :exclusions [org.clojure/java.classpath]]
                  [org.clojars.ninjudd/java.classpath "0.1.2-SNAPSHOT"]]
   :test-dependencies [[ring "0.3.8"]])
