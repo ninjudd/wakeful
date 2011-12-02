47e6c972c3d78e2c17275951aa98c578f3e0b0c3
Anthony Grimes <i@raynes.me>
2011-12-02 17:17:16 -0600

    Update dependencies.

--- project.clj
+++ project.clj
@@ -1,12 +1,13 @@
 (defproject wakeful "0.2.11"
   :description "restful routing alternative"
-  :dependencies [[clojure "1.2.0"]
+  :dependencies [[clojure "1.3.0"]
                  [useful "0.7.5-alpha3"]
-                 [clj-json "0.4.2"]
+                 [clj-json "0.4.3"]
                  [compojure "0.6.5"]
                  [ego "0.1.7"]
-                 [hiccup "0.3.5"]
+                 [hiccup "0.3.6"]
                  [classlojure "0.6.2"]
-                 [org.clojure/tools.namespace "0.1.1" :exclusions [org.clojure/java.classpath]]
-                 [org.clojars.ninjudd/java.classpath "0.1.2-SNAPSHOT"]]
-  :test-dependencies [[ring "0.3.8"]])
+                 [org.clojure/tools.namespace "0.1.1"
+                  :exclusions [org.clojure/java.classpath]]
+                 [org.clojure/java.classpath "0.2.0"]]
+  :dev-dependencies [[ring "1.0.0-RC4"]])
