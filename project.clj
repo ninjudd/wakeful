921b2fd556249f1b3679971a75d4449ec25cb295
Anthony Grimes <i@raynes.me>
2011-12-02 18:01:32 -0600

    No need for classlojure anymore.

--- project.clj
+++ project.clj
@@ -6,7 +6,6 @@
                  [compojure "0.6.5"]
                  [ego "0.1.7"]
                  [hiccup "0.3.6"]
-                 [classlojure "0.6.2"]
                  [org.clojure/tools.namespace "0.1.1"
                   :exclusions [org.clojure/java.classpath]]
                  [org.clojure/java.classpath "0.2.0"]]
