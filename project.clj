c5449fc91bcd61415d4a923f8a96109fad90208e
Justin Balthrop <git@justinbalthrop.com>
2012-01-06 12:56:45 -0800

    update ring, compojure, hiccup and clj-json

--- project.clj
+++ project.clj
@@ -1,12 +1,11 @@
-(defproject wakeful "0.2.12"
+(defproject wakeful "0.3.0"
   :description "restful routing alternative"
   :dependencies [[clojure "1.3.0"]
-                 [useful "0.7.5-alpha3"]
-                 [clj-json "0.4.3"]
-                 [compojure "0.6.5"]
+                 [useful "0.7.5"]
+                 [clj-json "0.5.0"]
+                 [compojure "1.0.0"]
+                 [ring "1.0.1"]
                  [ego "0.1.7"]
-                 [hiccup "0.3.6"]
-                 [org.clojure/tools.namespace "0.1.1"
-                  :exclusions [org.clojure/java.classpath]]
-                 [org.clojure/java.classpath "0.2.0"]]
-  :dev-dependencies [[ring "1.0.0-RC4"]])
+                 [hiccup "0.3.8"]
+                 [org.clojure/tools.namespace "0.1.1" :exclusions [org.clojure/java.classpath]]
+                 [org.clojure/java.classpath "0.2.0"]])
