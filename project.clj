ad7b8b9097aff44052dc348eefc5fefe4d1ffebd
Anthony Grimes <i@raynes.me>
2012-11-28 15:15:01 -0600

    Don't test against 1.3 anymore.

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
                  [compojure "1.1.3"]
                  [hiccup "1.0.1"]
                  [clj-json "0.5.0"]]
-  :aliases {"testall" ["with-profile" "dev,default:dev,1.3,default:dev,1.5,default" "test"]}
+  :aliases {"testall" ["with-profile" "dev,default:dev,1.5,default" "test"]}
   :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}}
   :repositories {"sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
